7a9ec3f3a71d301d255c6372dd487bb7c9d7c3c9
Einsrocket <einsrocket00@gmail.com>
2023-02-05 17:36:49 +0200

    new video playergs

--- src/App.jsx
+++ src/App.jsx
@@ -7,34 +7,34 @@ import "aos/dist/aos.css";
 import "./App.css";
 
 function App() {
-    const [endAnimation, setEndAnimation] = useState(false)
-    document.cookie = '__server=https://einsrocket-server.cyclic.app';
-    //document.cookie = '__server=http://localhost:3001';
-    
+    const [endAnimation, setEndAnimation] = useState(false);
+    document.cookie = "__server=https://einsrocket-server.cyclic.app";
+    // document.cookie = "__server=http://localhost:3001";
 
-    var cookies = document.cookie.split(';')
-        .map((cookie)=> cookie.split('='))
-        .reduce((accumulator, [key, value]) => ({...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
-
+    var cookies = document.cookie
+        .split(";")
+        .map((cookie) => cookie.split("="))
+        .reduce(
+            (accumulator, [key, value]) => ({
+                ...accumulator,
+                [key.trim()]: decodeURIComponent(value),
+            }),
+            {}
+        );
 
     useEffect(() => {
         Aos.init({ duration: 600 });
     }, []);
 
-    
     // close initial animation
     setTimeout(() => {
         setEndAnimation(true);
-    },1500);
-
+    }, 1500);
 
     return (
         <MainContext>
             <div className="app">
-                {
-                    endAnimation ? <Router /> : <InitialAnimation />
-                }
-                
+                {endAnimation ? <Router /> : <InitialAnimation />}
             </div>
         </MainContext>
     );
